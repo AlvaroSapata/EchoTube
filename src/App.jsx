f2e9ecaf72f58a9124a7980c37469fbd5ac4bcc4
Alvaro Martinez <alvaromfdv@gmail.com>
2025-01-30 19:21:58 +0100

    Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Video from "./Pages/Video/Video";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/video/:category/:videoId" element={<Video />} />
+      </Routes>
     </div>
   );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Home.css";
+import Sidebar from "../../Components/Sidebar/Sidebar";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
 };
 
 export default Home;
