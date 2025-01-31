0500dbfb3450c210f38cf2e4af9612c112e70781
Alvaro Martinez <alvaromfdv@gmail.com>
2025-01-31 10:09:22 +0100

    Feed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Home.css";
 import Sidebar from "../../Components/Sidebar/Sidebar";
+7;
+import Feed from "../../Components/Feed/Feed";
 
-export const Home = () => {
+export const Home = ({ sidebar }) => {
   return (
     <>
-      <Sidebar />
+      <Sidebar sidebar={sidebar} />
+      <div className={`container ${sidebar ? "" : "large-container"}`}>
+        <Feed />
+      </div>
     </>
   );
 };
