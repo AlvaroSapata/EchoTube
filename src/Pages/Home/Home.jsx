9e836582a2df76ca7a655aec858056de6df6e72c
Alvaro Martinez <alvaromfdv@gmail.com>
2025-01-31 17:37:11 +0100

    YT API + Home Videos and categories

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "./Home.css";
 import Sidebar from "../../Components/Sidebar/Sidebar";
-7;
 import Feed from "../../Components/Feed/Feed";
+import { useState } from "react";
 
 export const Home = ({ sidebar }) => {
+  const [category, setCategory] = useState(0);
+
   return (
     <>
-      <Sidebar sidebar={sidebar} />
+      <Sidebar
+        sidebar={sidebar}
+        category={category}
+        setCategory={setCategory}
+      />
       <div className={`container ${sidebar ? "" : "large-container"}`}>
-        <Feed />
+        <Feed category={category}/>
       </div>
     </>
   );
